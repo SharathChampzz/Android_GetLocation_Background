ad230f4ccf9218ea2963d809515a2ba1d156ec93
SharathChampzz <sharathkumar.mskj.1999@gmail.com>
2020-11-23 13:23:27 +0530

    first commit

---
+++ 1BackGround/app/src/main/java/com/example/backgroundloc/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.backgroundloc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    static MainActivity instance;
+    LocationRequest locationRequest;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    TextView textView;
+
+    public static MainActivity getInstance() {
+        return instance;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.cordinates);
+
+        instance = this;
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        updateLocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this, "You must Grant Permission to make it work!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+    private void updateLocation() {
+        buildLocationRequest();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, getPendingIntent());
+
+    }
+
+    private PendingIntent getPendingIntent() {
+        Intent intent = new Intent(this, MyLocationService.class);
+        intent.setAction(MyLocationService.ACTION_PROCESS_UPDATE);
+        return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setSmallestDisplacement(1f);
+    }
+
+    public void updateTextView(String s){
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(s);
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
---
+++ 1BackGround/app/src/main/java/com/example/backgroundloc/MyLocationService.java
@@ -0,0 +1,40 @@
+package com.example.backgroundloc;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationResult;
+
+public class MyLocationService extends BroadcastReceiver {
+
+    public static final String ACTION_PROCESS_UPDATE = "com.example.backgroundloc.UPDATE_LOCATION";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent != null){
+            final String action = intent.getAction();
+
+            if(ACTION_PROCESS_UPDATE.equals(action)){
+                LocationResult result = LocationResult.extractResult(intent);
+
+                if(result!=null){
+                    Location location = result.getLastLocation();
+                    String locstring = "" + location.getLatitude() + " , " + location.getLongitude();
+                    try{
+                    MainActivity.getInstance().updateTextView(locstring);
+                    }
+                    catch (Exception e){
+                        Toast.makeText(context, locstring, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+
+}
